b78ff3985448dc0ff91b8be5358bba348b7f40b4
tim <tim_stehle@posteo.de>
2021-07-14 01:35:19 +0200

    deploy contract

---
+++ solidity-wallet/migrations/2_deploy_contracts.js
@@ -0,0 +1,8 @@
+const MultiSigWallet = artifacts.require("MultiSigWallet");
+
+module.exports = function (deployer, network, accounts) {
+    const owners = accounts.slice(0,3)
+    const numConfirmationsRequired = 2
+    
+    deployer.deploy(MultiSigWallet, owners, numConfirmationsRequired);
+};
